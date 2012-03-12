fea360ea50d41ded9d660081d3b8ce8ac5d6bade
Ed Eustace <edeustace@Eds-MacBook-Pro.local>
2012-03-12 20:20:02 +0000

    fix app due to RC3 release

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,8 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "postgresql" % "postgresql" % "9.1-901.jdbc4"
+      "postgresql" % "postgresql" % "9.1-901.jdbc4",
+      "org.scala-lang" % "scala-compiler" % "2.9.1"
 
     )
 
